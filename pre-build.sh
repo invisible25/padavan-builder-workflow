1a630ffc493198bed59a2dbd5f1183144952e270
invisible25 <77500366+invisible25@users.noreply.github.com>
2025-12-02 12:46:57 +0500

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -3,11 +3,11 @@ set -e
 
 cd padavan-ng/trunk/user/openvpn/openvpn-2.6.15
 
-wget https://github.com/samanajd2/OpenVPN-XOR-windows/blob/master/Xor-Patches/02-tunnelblick-openvpn_xorpatch-a.diff
-wget https://github.com/samanajd2/OpenVPN-XOR-windows/blob/master/Xor-Patches/03-tunnelblick-openvpn_xorpatch-b.diff
-wget https://github.com/samanajd2/OpenVPN-XOR-windows/blob/master/Xor-Patches/04-tunnelblick-openvpn_xorpatch-c.diff
-wget https://github.com/samanajd2/OpenVPN-XOR-windows/blob/master/Xor-Patches/05-tunnelblick-openvpn_xorpatch-d.diff
-wget https://github.com/samanajd2/OpenVPN-XOR-windows/blob/master/Xor-Patches/06-tunnelblick-openvpn_xorpatch-e.diff
+wget https://raw.githubusercontent.com/Tunnelblick/Tunnelblick/master/third_party/sources/openvpn/openvpn-2.6.14/patches/02-tunnelblick-openvpn_xorpatch-a.diff
+wget https://raw.githubusercontent.com/Tunnelblick/Tunnelblick/master/third_party/sources/openvpn/openvpn-2.6.14/patches/03-tunnelblick-openvpn_xorpatch-b.diff
+wget https://raw.githubusercontent.com/Tunnelblick/Tunnelblick/master/third_party/sources/openvpn/openvpn-2.6.14/patches/04-tunnelblick-openvpn_xorpatch-c.diff
+wget https://raw.githubusercontent.com/Tunnelblick/Tunnelblick/master/third_party/sources/openvpn/openvpn-2.6.14/patches/05-tunnelblick-openvpn_xorpatch-d.diff
+wget https://raw.githubusercontent.com/Tunnelblick/Tunnelblick/master/third_party/sources/openvpn/openvpn-2.6.14/patches/06-tunnelblick-openvpn_xorpatch-e.diff
 
 patch -p1 < 02-tunnelblick-openvpn_xorpatch-a.diff
 patch -p1 < 03-tunnelblick-openvpn_xorpatch-b.diff
